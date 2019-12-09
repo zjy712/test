a61585c0502295e89311c9cd0127ac5cb1b013d2
jayzhu <jayzhu@capvision.com>
2019-12-09 18:21:35 +0800

    class

--- ts/test1/src/index.js
+++ ts/test1/src/index.js
@@ -1,4 +1,4 @@
-var hello = 'hello TypeScript';
+var hello = 'hello TypeScrit';
 
 document.querySelectorAll('.app')[0].innerHTML = hello;
 console.log(document.querySelectorAll('.app')[0]);
--- ts/test1/src/index.ts
+++ ts/test1/src/index.ts
@@ -6,3 +6,31 @@ console.log(document.querySelectorAll('.app')[0]);
 interface StringArray {
     [index: number]: string
 }
+
+class Dog {
+  constructor(name: string) {
+    this.name = name
+  }
+  public name: string = 'dog'
+  run() {console.log('Dog run')}
+  private pri() {console.log('Dog  pri')}
+  protected pro() {console.log('Dog pro')}
+  readonly legs: number = 4
+}
+
+
+let dog = new Dog('wangwang');
+
+console.log(dog.legs);
+
+class Husky extends Dog {
+  constructor (name: string ,color:string) {
+    super(name)
+    this.color = color;
+    this.pro()
+  }
+  color:string
+}
+let husky = new Husky('Husky','black')
+
+console.log(husky.legs);
